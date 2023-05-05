ffa2aa9d53892132978d35b517f9c7770eb8ba87
TristeTempsChang <tristan.tran2111@gmail.com>
2023-05-05 13:42:54 +0200

    Dossier d'installation

--- GestionStock/Program.cs
+++ GestionStock/Program.cs
@@ -16,7 +16,7 @@ namespace GestionStock
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1()); 
+            Application.Run(new Demarrage()); 
         }
     }
 }
